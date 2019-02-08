f5e61ea4aa0df977b5767514eea6d82fa06bdd48
inimitableharish <inimitableharish@gmail.com>
2019-02-08 15:34:51 +0530

    removed the comments, added base path

--- classifier.py
+++ classifier.py
@@ -5,6 +5,13 @@ from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import pickle
+import os
+
+MODEL_FILE_NAME = 'nbclassifier.sav'
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+DATA_FILE = BASE_PATH + os.sep + 'data.csv'
 
 
 def train_nb(x_train, y_train):
@@ -13,9 +20,14 @@ def train_nb(x_train, y_train):
     labels'''
     gnb = GaussianNB()
     gnb.fit(x_train, y_train)
-    y_test = np.array([[2, 2, 0, 0]])
+    pickle.dump(gnb, open(MODEL_FILE_NAME, 'wb'))  # Since gnb is our model
 
-    predicted_value = gnb.predict(y_test)
+
+def predict(y_test=[2, 2, 0, 0]):
+    '''Takes a list as input'''
+    loaded_gnb_model = pickle.load(open(MODEL_FILE_NAME, 'rb'))
+    y_test = np.array([y_test])
+    predicted_value = loaded_gnb_model.predict(y_test)
     print('Predicted value', predicted_value)
 
 
@@ -27,32 +39,11 @@ def load_data(data_file='data.csv'):
     y_train = np.array(y_train)
     x_train = np.array(x_train)
     y_train = y_train.reshape(-1, 1)
-    # ohe = OneHotEncoder(sparse=False)
-    # y_train = ohe.fit_transform(y_train)
-    print('Shape of x', x_train.shape)
-    print('Shape of y', y_train.shape)
-
-    return x_train, y_train
-
-    '''
-    with open(data_file, 'r') as file_handle:
-        for lines in file_handle.readlines():
-            lines = lines.strip('\n').split(',')
-            # print(y_train, lines[-1])
-            np.append(y_train, lines[-1])
-            np.append(x_train, lines[:-1])
-    '''
-
     return x_train, y_train
 
 
-x_train, y_train = load_data()
+x_train, y_train = load_data(DATA_FILE)
 
 
-# print('x_train -> ', x_train)
-# print('y_train -> ', y_train)
-
-
-for x, y in zip(x_train, y_train):
-    print(x, y)
 train_nb(x_train, y_train)
+predict(y_test=[2, 2, 0, 0])
