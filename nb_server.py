3fca6494ff792400c778c80672b17227f1124c19
inimitableharish <inimitableharish@gmail.com>
2019-02-08 20:17:21 +0530

    grpc server implemented

--- nb_server.py
+++ nb_server.py
@@ -1,4 +1,36 @@
-from protobuffpy import nb_pb2
+import nb_pb2
+import nb_pb2_grpc
+from concurrent import futures
+import logging
+import grpc
+import time
+
+ONE_DAY_IN_SEC = 12 * 60 * 60
 
 
-class TrainServicer(nb_pb2.TrainServicer):
+class TrainServicer(nb_pb2_grpc.TrainServicer):
+    def __init__(self):
+        pass
+
+    def TrainClassifier(self, request, context):
+        print('context', context)
+
+
+def serve():
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    nb_pb2_grpc.add_TrainServicer_to_server(
+        TrainServicer(), server)
+    server.add_insecure_port('[::]:8080')
+    server.start()
+    try:
+        while True:
+            print('server listening at 8080')
+            time.sleep(ONE_DAY_IN_SEC)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
